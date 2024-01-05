640f81dd753429e6a598fbfc144326fcbced65cc
kandath haneesh <70889560+Haneesh3@users.noreply.github.com>
2024-01-05 13:01:13 +0530

    Add files via upload

---
+++ binary search tree!!!.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int value) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node *insert(struct Node *root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+struct Node *minValueNode(struct Node *node) {
+    struct Node *current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+struct Node *deleteNode(struct Node *root, int value) {
+    if (root == NULL)
+        return root;
+
+    if (value < root->data)
+        root->left = deleteNode(root->left, value);
+    else if (value > root->data)
+        root->right = deleteNode(root->right, value);
+    else {
+        if (root->left == NULL) {
+            struct Node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        struct Node *temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+struct Node *search(struct Node *root, int value) {
+    if (root == NULL || root->data == value)
+        return root;
+
+    if (value < root->data)
+        return search(root->left, value);
+    else
+        return search(root->right, value);
+}
+
+void inorderTraversal(struct Node *root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct Node *root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    int searchValue = 40;
+    struct Node *searchResult = search(root, searchValue);
+    if (searchResult != NULL)
+        printf("%d found in the tree.\n", searchValue);
+    else
+        printf("%d not found in the tree.\n", searchValue);
+
+    int deleteValue = 30;
+    root = deleteNode(root, deleteValue);
+    printf("Inorder traversal after deletion: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
